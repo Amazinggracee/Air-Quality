1e9bebebbb32e2aeab28cb9f0380af41b99ee900
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-05-26 20:51:53 +0100

    Updates

--- src/__test__/PageItems.test.js
+++ src/__test__/PageItems.test.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from '../redux/configureStore';
-import PaginatedItems from '../pages/PageItems';
+import PageItems from '../pages/PageItems';
 
 describe('testing PageItems component', () => {
   it('should render the PageItems component', () => {
     const paginatedItems = render(
       <Provider store={store}>
         <BrowserRouter>
-          <PaginatedItems />
+          <PageItems />
         </BrowserRouter>
       </Provider>,
     );
